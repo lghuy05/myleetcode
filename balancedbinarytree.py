ea48ab5d91e82fb132f2caf684669f6f24d41d03
lghuy05 <luonggiahuy1909@gmail.com>
2025-11-15 08:41:58 -0500

    grind

---
+++ balancedbinarytree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        left_height = self.dfs(root.left)
+        right_height = self.dfs(root.right)
+
+        return (
+            abs(left_height - right_height) <= 1
+            and self.isBalanced(root.left)
+            and self.isBalanced(root.right)
+        )
+
+    def dfs(self, node):
+        if not node:
+            return 0
+        return 1 + max(self.dfs(node.left), self.dfs(node.right))
