c6fa87c6f805c658bacffba73c182075ec5146b6
lghuy05 <luonggiahuy1909@gmail.com>
2025-11-23 09:42:50 -0500

    grind

--- carfleet.py
+++ carfleet.py
@@ -3,19 +3,34 @@ from typing import List
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        result: int = 0
-        n: int = len(position)
-        time_li: List[int] = []
-        max_time: int = 0
-        cars: List[tuple] = sorted(zip(position, speed), reverse=True)
-        for position, speed in cars:
-            time: int = (target - position) / speed
-            if time > max_time:
-                result += 1
-                max_time = time
+        # result: int = 0
+        # n: int = len(position)
+        # time_li: List[int] = []
+        # max_time: int = 0
+        # cars: List[tuple] = sorted(zip(position, speed), reverse=True)
+        # for position, speed in cars:
+        #     time: int = (target - position) / speed
+        #     if time > max_time:
+        #         result += 1
+        #         max_time = time
+        #
+        # return result
+        cars = sorted(zip(position, speed))  # Farthest to closest
+        stack = []
 
-        return result
+        for pos, spd in cars:
+            time = (target - pos) / spd
+
+            # Remove cars from stack that this car will catch up to
+            while stack and time >= stack[-1]:
+                stack.pop()
+
+            # Add this car's time to stack
+            stack.append(time)
+
+        return len(stack)
 
 
 sol = Solution()
-print(sol.carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
+print(sol.carFleet(10, [1, 4], [3, 2]))
+print(sol.carFleet(10, [4, 1, 0, 7], [2, 2, 1, 1]))
