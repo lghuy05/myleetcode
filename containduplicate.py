78dabac98b1e3a792ce929d95b6337674b2a731f
lghuy05 <luonggiahuy1909@gmail.com>
2025-11-26 15:53:01 -0500

    grind

--- containduplicate.py
+++ containduplicate.py
@@ -3,9 +3,24 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        # O(1)
-        if len(set(nums)) == len(nums):
-            return True
+        # O(n) time complexity, O(n) space due to new set memory allocation
+        # if len(set(nums)) == len(nums):
+        #     return False
+        # return True
+
+        # O(n) time complexity, O(n) space worst scenario but more effiect due to early return if True
+        # seen = set()
+        # for i in nums:
+        #     if i in seen:
+        #         return True
+        #     seen.add(i)
+        # return False
+
+        # O(nlogn) time complexity, O(1) space complexity
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
         return False
 
 
