7afd1f92b123bc1768de1b0d04b126a08c62df6c
lghuy05 <luonggiahuy1909@gmail.com>
2025-12-20 11:28:37 -0500

    grind

--- deletecoloumnstomakesorted.py
+++ deletecoloumnstomakesorted.py
@@ -3,18 +3,30 @@ from typing import List
 
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
+        # count = 0
+        # columns = len(strs[0])
+        # rows = len(strs)
+        # for i in range(columns):
+        #     li = [0] * 26
+        #     last = -1
+        #     for j in range(rows):
+        #         if ord(strs[j][i]) < last:
+        #             count += 1
+        #             break
+        #         li[ord(strs[j][i]) - ord("a")] += 1
+        #         last = max(last, ord(strs[j][i]))
+        # return count
+        #
         count = 0
         columns = len(strs[0])
         rows = len(strs)
         for i in range(columns):
-            li = [0] * 26
-            last = -1
+            last = "a"
             for j in range(rows):
-                if ord(strs[j][i]) < last:
+                if strs[j][i] < last:
                     count += 1
                     break
-                li[ord(strs[j][i]) - ord("a")] += 1
-                last = max(last, ord(strs[j][i]))
+                last = strs[j][i]
         return count
 
 
