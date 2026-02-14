1a4d36d3263e3cefb6381ef77880c071f140582f
lghuy05 <luonggiahuy1909@gmail.com>
2026-02-14 13:35:09 -0500

    grind

---
+++ c++/longestrepeatingcharacterreplacement.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+class Solution {
+public:
+  int characterReplacement(string s, int k) {
+    vector<int> freq(26, 0);
+    int left = 0, right = 0;
+    int maxLen = 0;
+    int maxFreq = 0;
+    while (right < s.size()) {
+      freq[s[right] - 'A']++;
+      maxFreq = max(maxFreq, freq[s[right] - 'A']);
+      while ((right - left) + 1 - maxFreq > k) {
+        freq[s[left] - 'A']--;
+        left++;
+      }
+      maxLen = max(maxLen, right - left + 1);
+      right++;
+    }
+    return maxLen;
+  }
+};
