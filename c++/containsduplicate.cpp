fc48899a18e15675396442c9d731ae3fb0c6193c
lghuy05 <luonggiahuy1909@gmail.com>
2026-02-18 11:59:08 -0500

    grind

---
+++ c++/containsduplicate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+class Solution {
+public:
+  bool containsNearbyDuplicate(vector<int> &nums, int k) {
+    unordered_set<int> window;
+    for (int i = 0; i < nums.size(); i++) {
+      if (window.count(nums[i])) {
+        return true;
+      }
+      window.insert(nums[i]);
+      if (window.size() > k) {
+        window.erase(nums[i - k]);
+      }
+    }
+    return false;
+  }
+};
