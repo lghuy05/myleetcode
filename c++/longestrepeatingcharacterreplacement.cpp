0ec9b2a4b002824a28cc3e5188985772bf1b7f44
lghuy05 <luonggiahuy1909@gmail.com>
2026-02-25 09:53:02 -0500

    grind

--- c++/longestrepeatingcharacterreplacement.cpp
+++ c++/longestrepeatingcharacterreplacement.cpp
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
+using namespace std;
 class Solution {
 public:
   int characterReplacement(string s, int k) {
+    // TC: O(n)
+    int ans = 0;
     vector<int> freq(26, 0);
-    int left = 0, right = 0;
-    int maxLen = 0;
-    int maxFreq = 0;
-    while (right < s.size()) {
+    int left = 0;
+    int check = 0;
+    for (int right = 0; right < s.size(); right++) {
       freq[s[right] - 'A']++;
-      maxFreq = max(maxFreq, freq[s[right] - 'A']);
-      while ((right - left) + 1 - maxFreq > k) {
+      check = max(check, freq[s[right] - 'A']);
+      while ((right - left + 1) - check > k) {
         freq[s[left] - 'A']--;
         left++;
       }
-      maxLen = max(maxLen, right - left + 1);
-      right++;
+      ans = max(ans, right - left + 1);
     }
-    return maxLen;
+
+    return ans;
   }
 };
