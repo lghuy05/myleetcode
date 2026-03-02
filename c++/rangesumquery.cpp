be9d698c349d5d55b170915a08ba5c1983a01ec6
lghuy05 <luonggiahuy1909@gmail.com>
2026-03-02 14:40:10 -0500

    grind

---
+++ c++/rangesumquery.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+class NumArray {
+private:
+  vector<int> prefix;
+
+public:
+  NumArray(vector<int> &nums) {
+    prefix.resize(nums.size() + 1, 0);
+    for (int i = 1; i < prefix.size(); i++) {
+      prefix[i] = prefix[i - 1] + nums[i - 1];
+    }
+  }
+
+  int sumRange(int left, int right) { return prefix[right + 1] - prefix[left]; }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
