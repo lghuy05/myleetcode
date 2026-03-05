d327827513efe9c5f0fdc4ad148b28cbf7e87066
lghuy05 <luonggiahuy1909@gmail.com>
2026-03-04 21:30:32 -0500

    grind

--- c++/draft.cpp
+++ c++/draft.cpp
@@ -11,13 +11,6 @@ void worker(int id) {
 }
 
 int main() {
-  int n = 4;
-  std::vector<std::thread> threads;
-
-  for (int i = 0; i < n; i++) {
-    threads.emplace_back(worker, i);
-  }
-
-  for (auto &t : threads)
-    t.join();
+  std::cout << "Hi";
+  return 0;
 }
