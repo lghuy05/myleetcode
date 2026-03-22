751db1bd0b46b0d0d043f6c15a8f8130c20f9533
lghuy05 <luonggiahuy1909@gmail.com>
2026-03-22 09:47:55 -0400

    grind

--- c++/validpalidrome.cpp
+++ c++/validpalidrome.cpp
@@ -1,34 +1,29 @@
-#include <bits/stdc++.h>
-
-using namespace std;
 class Solution {
 public:
-  bool check(int n) {
-    return (n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'Z') ||
-           (n >= '0' && n <= '9');
+  bool check(char s) {
+    if ((s >= 'A' && s <= 'Z') || (s >= 'a' && s <= 'z') ||
+        (s >= '0' && s <= '9'))
+      return true;
+    else {
+      return false;
+    }
   }
 
   bool isPalindrome(string s) {
-
     int left = 0;
     int right = s.size() - 1;
-
     while (left < right) {
-
       while (left < right && !check(s[left]))
         left++;
       while (left < right && !check(s[right]))
         right--;
 
       if (s[left] >= 'A' && s[left] <= 'Z')
-        s[left] += ('a' - 'A');
-
+        s[left] += 'a' - 'A';
       if (s[right] >= 'A' && s[right] <= 'Z')
-        s[right] += ('a' - 'A');
-
+        s[right] += 'a' - 'A';
       if (s[left] != s[right])
         return false;
-
       left++;
       right--;
     }
