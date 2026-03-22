17dec70bd28de372357b274ad949140e0917e716
lghuy05 <luonggiahuy1909@gmail.com>
2026-03-22 12:45:38 -0400

    grind

--- c++/threesum.cpp
+++ c++/threesum.cpp
@@ -1,32 +1,27 @@
-#include <bits/stdc++.h>
-
-using namespace std;
 class Solution {
 public:
   vector<vector<int>> threeSum(vector<int> &nums) {
-    vector<vector<int>> res;
-
     int n = nums.size();
-
+    vector<vector<int>> res;
     sort(nums.begin(), nums.end());
-    for (int i = 0; i < n; i++) {
-      if (i > 0 && nums[i] == nums[i - 1])
-        continue;
+    for (int i = 0; i < n - 2; i++) {
       int left = i + 1;
       int right = n - 1;
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
       while (left < right) {
-        int sum = nums[left] + nums[i] + nums[right];
-        if (sum == 0) {
-          res.push_back({nums[left], nums[i], nums[right]});
+        int sum = nums[i] + nums[left] + nums[right];
+        if (sum < 0) {
           left++;
+        } else if (sum > 0) {
           right--;
-          while (left < right && nums[left] == nums[left - 1])
+        } else if (sum == 0) {
+          res.push_back({nums[i], nums[left], nums[right]});
+          while (left < right && nums[left + 1] == nums[left])
             left++;
-          while (left < right && nums[right] == nums[right + 1])
+          while (left < right && nums[right - 1] == nums[right])
             right--;
-        } else if (sum < 0) {
           left++;
-        } else {
           right--;
         }
       }
